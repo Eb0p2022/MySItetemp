8d88744f9538d1f3c8dbe678c1909553fc0e72ea
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2019-12-29 15:07:01 +0100

    project start

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+    app = express(),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    mongoose = require('mongoose');
+
+//  ====    DB Setup    ====
+mongoose.connect('mongodb://localhost/the_den', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use('/public', express.static(path.join(__dirname, "/public")));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen('4000', () => {
+    console.log('The Den Server has started...');
+})
